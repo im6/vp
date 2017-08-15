6a77d1e39d726fc0fceba01e88f5039d0d490bf3
jinouwuque <ee2win@gmail.com>
2017-08-15 00:14:44 -0400

    add download function

--- client/misc/util.js
+++ client/misc/util.js
@@ -31,3 +31,44 @@ export function scrollTop() {
     window.scrollTo(0, 0);
   }
 }
+
+export function downloadCanvas(color) {
+  const HEIGHT = 340,
+    WIDTH = 340,
+    MARGIN = 13,
+    CANVASRATIO = 0.8,
+    BOTTOMRADIO = 5;
+  const colors = color.split('#');
+  const myCanvas = document.createElement('canvas');
+  const ctx = myCanvas.getContext('2d');
+  myCanvas.id = "tempCanvas";
+  myCanvas.width = WIDTH;
+  myCanvas.height = HEIGHT;
+  myCanvas.style = 'border: 1px solid #c1c1c1; border-radius: 10px;';
+  myCanvas.toDataURL();
+
+  const boxHts = [
+    HEIGHT * CANVASRATIO * 0.4,
+    HEIGHT * CANVASRATIO * 0.25,
+    HEIGHT * CANVASRATIO * 0.175,
+    HEIGHT * CANVASRATIO * 0.175,
+  ];
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(MARGIN,MARGIN,WIDTH - (MARGIN * 2), boxHts[0]);
+  ctx.fillStyle = "#0F0F00";
+  ctx.fillRect(MARGIN,MARGIN + boxHts[0],WIDTH - (MARGIN * 2), boxHts[1]);
+  ctx.fillStyle = "#0000FF";
+  ctx.fillRect(MARGIN,MARGIN + boxHts[0] + boxHts[1],WIDTH - (MARGIN * 2), boxHts[2]);
+  ctx.fillStyle = "#00FF00";
+  ctx.fillRect(MARGIN,MARGIN + boxHts[0] + boxHts[1] + boxHts[2],WIDTH - (MARGIN * 2), boxHts[3]);
+
+  ctx.font = "28px";
+  ctx.fillStyle = "#485f92";
+  ctx.fillText("ColorPK.com", MARGIN, MARGIN * 3 + HEIGHT - (MARGIN * BOTTOMRADIO));
+  ctx.fillText("ColorPK.com", MARGIN, MARGIN * 3 + HEIGHT - (MARGIN * BOTTOMRADIO));
+
+
+
+
+  document.body.appendChild(myCanvas);
+}
