fb3f92b825b218a12bcb5ecdf0257f9c47e7421a
zj1926 <zj1926@users.noreply.github.com>
2017-08-15 17:03:06 -0400

    Update util.js

--- client/misc/util.js
+++ client/misc/util.js
@@ -33,18 +33,18 @@ export function scrollTop() {
 }
 
 export function downloadCanvas(color) {
-  const HEIGHT = 340,
+  const HEIGHT = 380,
     WIDTH = 340,
     MARGIN = 13,
-    CANVASRATIO = 0.8,
+    CANVASRATIO = 0.7,
     BOTTOMRADIO = 5;
-  const colors = color.split('#');
+  const colors = color.split('#').map(v => '#'+v);
   const myCanvas = document.createElement('canvas');
   const ctx = myCanvas.getContext('2d');
   myCanvas.id = "tempCanvas";
   myCanvas.width = WIDTH;
   myCanvas.height = HEIGHT;
-  myCanvas.style = 'border: 1px solid #c1c1c1; border-radius: 10px;';
+  myCanvas.style = 'border: 1px solid #c1c1c1';
   myCanvas.toDataURL();
 
   const boxHts = [
@@ -53,19 +53,31 @@ export function downloadCanvas(color) {
     HEIGHT * CANVASRATIO * 0.175,
     HEIGHT * CANVASRATIO * 0.175,
   ];
-  ctx.fillStyle = "#FF0000";
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(0,0, WIDTH, HEIGHT);
+  
+  ctx.fillStyle = colors[0];
   ctx.fillRect(MARGIN,MARGIN,WIDTH - (MARGIN * 2), boxHts[0]);
-  ctx.fillStyle = "#0F0F00";
+  ctx.fillStyle = colors[1];
   ctx.fillRect(MARGIN,MARGIN + boxHts[0],WIDTH - (MARGIN * 2), boxHts[1]);
-  ctx.fillStyle = "#0000FF";
+  ctx.fillStyle = colors[2];
   ctx.fillRect(MARGIN,MARGIN + boxHts[0] + boxHts[1],WIDTH - (MARGIN * 2), boxHts[2]);
-  ctx.fillStyle = "#00FF00";
+  ctx.fillStyle = colors[3];
   ctx.fillRect(MARGIN,MARGIN + boxHts[0] + boxHts[1] + boxHts[2],WIDTH - (MARGIN * 2), boxHts[3]);
 
-  ctx.font = "28px";
-  ctx.fillStyle = "#485f92";
-  ctx.fillText("ColorPK.com", MARGIN, MARGIN * 3 + HEIGHT - (MARGIN * BOTTOMRADIO));
-  ctx.fillText("ColorPK.com", MARGIN, MARGIN * 3 + HEIGHT - (MARGIN * BOTTOMRADIO));
+	const colorTxtPosition = CANVASRATIO* HEIGHT + (MARGIN * 3),
+  space = 17;
+  
+	ctx.font = '13px Arial';
+  ctx.fillStyle = "#a3a3a3";
+  ctx.fillText('ColorPK.com', WIDTH - MARGIN - 78,colorTxtPosition + space * 3);  
+  
+  ctx.font = '15px Arial';
+  ctx.fillStyle = '#218bbc';
+  ctx.fillText(colors[0], MARGIN, colorTxtPosition);  
+  ctx.fillText(colors[1], MARGIN, colorTxtPosition + space);
+  ctx.fillText(colors[2], MARGIN, colorTxtPosition + space * 2);
+  ctx.fillText(colors[3], MARGIN, colorTxtPosition + space * 3);
 
 
 
