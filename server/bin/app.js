7c97501cafa5ccd762dad84d95067841b85ab308
jinouwuque <ee2win@gmail.com>
2017-09-26 22:14:42 -0400

    change env name again

--- server/bin/app.js
+++ server/bin/app.js
@@ -26,8 +26,8 @@ var sessionOpt = {
 
 if(!globalConfig.isDev){
   sessionOpt.store = new MySQLStore({
-    host     : process.env['SQL_DB_HOST'],
-    port     : process.env['SQL_DB_PORT'],
+    host     : process.env['SQL_HOST'],
+    port     : process.env['SQL_PORT'],
     user     : process.env['SQL_USERNAME'],
     password : process.env['SQL_PASSWORD'],
     database : process.env['SQL_DATABASE'],
--- server/resource/db/mysqlConnection.js
+++ server/resource/db/mysqlConnection.js
@@ -7,8 +7,8 @@ module.exports = {
     pool = mysql.createPool({
       connectionLimit : 8,
       multipleStatements: false,
-      host     : process.env['SQL_DB_HOST'],
-      port     : process.env['SQL_DB_PORT'],
+      host     : process.env['SQL_HOST'],
+      port     : process.env['SQL_PORT'],
       user     : process.env['SQL_USERNAME'],
       password : process.env['SQL_PASSWORD'],
       database : process.env['SQL_DATABASE']
