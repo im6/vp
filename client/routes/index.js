cecb1c6e4ad136661952802e885ff52e00dc2e9c
im6 <ee2win@gmail.com>
2019-04-07 19:52:01 -0400

    update the route

--- client/routes/index.js
+++ client/routes/index.js
@@ -1,15 +1,15 @@
 import React, { Fragment } from 'react';
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Color from '../modules/color';
 import About from '../modules/about';
 
 class Routes extends React.Component {
   render(){
-    return <Fragment>
+    return <Switch>
       <Route exact path="/" component={Color} />
       <Route path="/popular" component={Color} />
       <Route path="/about" component={About} />
-    </Fragment>
+    </Switch>
   }
 }
 
