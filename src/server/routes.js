997c07f40509e8994dc6b5433366d6dac607aa6b
im6 <ee2win@gmail.com>
2019-04-08 21:35:53 -0400

    remove src

--- src/server/routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { initColorLatest } from './controllers/main';
-
-const router = express.Router();
-
-router.get('/latest', initColorLatest);
-
-export default router;
