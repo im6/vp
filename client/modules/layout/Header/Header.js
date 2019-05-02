93316e3b96c43dd5c32bdaef72f8d1004d260898
im6 <ee2win@gmail.com>
2019-05-02 19:43:45 -0400

    update and remove unused

--- client/modules/layout/Header/Header.js
+++ client/modules/layout/Header/Header.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { ISMOBILE } from '../../../config/global';
-import { Menu, Icon, Button, Dropdown } from 'antd';
 import { Link } from 'react-router-dom';
-import ProfileMenu from '../ProfileMenu';
 import style from './style.sass';
+
 class Header extends React.Component {
   constructor(props){
     super(props)
@@ -130,79 +128,6 @@ class Header extends React.Component {
       </div>
     </div>
   </nav>)
-  }
-
-  render_old() {
-    const { url, isAuth, detail } = this.props;
-    const profileMenu = isAuth ?
-    <ProfileMenu
-      onClickLogout={this.onLogout}
-      onClickProfile={this.props.onEnterProfile}
-      username={detail.get('name')} /> : null;
-    const menu = <div className={style.loginBox}>
-      <img className={this.props.authReady? '' : style.disableClick}
-        src="//dkny.oss-cn-hangzhou.aliyuncs.com/4/fb.png"
-        onClick={this.onFBClick}
-      />
-    </div>
-    
-    return (
-      <div className={style.lock}>
-        <Menu
-          onClick={this.handleClick}
-          selectedKeys={[this.props.url]}
-          mode="horizontal"
-        >
-          <Menu.Item key="/popular">
-            <Link to="/popular">
-              <Icon type="rise" />
-              Popular
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/">
-            <Link to="/">
-              <Icon type="clock-circle" />
-              Latest
-            </Link>
-          </Menu.Item>
-          {
-            !ISMOBILE && <Menu.Item key="/about">
-              <Link to="/about">
-                <Icon type="info-circle" />
-                About
-              </Link>
-            </Menu.Item>
-          }
-          
-        </Menu>
-        <div className={style.buttonGroup}>
-        {
-            url !== '/new' && <Link to="/new">
-              <Button type="primary" icon="plus">
-                { ISMOBILE ? null: 'New Color'}
-              </Button>
-            </Link>
-          }
-          &nbsp;&nbsp;
-          {
-            isAuth ?
-            (<Dropdown overlay={profileMenu}>
-              <img src={detail.get('img')} alt="icon"/>
-            </Dropdown>) :
-            (<Dropdown
-              overlay={menu}
-              // onVisibleChange={this.onClickLogin}
-              trigger={["click"]}
-            >
-                <Button icon="user">
-                  { ISMOBILE ? null: 'Sign In'}
-                </Button>
-            </Dropdown>)
-          }
-          
-        </div>
-      </div>
-    );
   }
 }
 
