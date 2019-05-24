c80aae184e060c332e2cb3d414def5487cc116e9
im6 <5487482+im6@users.noreply.github.com>
2019-05-24 14:36:48 -0400

    Update oauth.js

--- server/resource/oauth.js
+++ server/resource/oauth.js
@@ -2,20 +2,18 @@ import axios from 'axios';
 import { fbApi as baseURL } from '../config';
 
 export const accessToken = (params) => {
-  const req = axios({
+  return axios({
     baseURL,
     method: 'get',
     url: '/oauth/access_token',
     params
-  })
-  return req;
+  });
 }
 export const showUser = (params) => {
-  const req = axios({
+  return axios({
     baseURL,
     method: 'get',
     url: '/me',
     params
-  })
-  return req;
+  });
 }
