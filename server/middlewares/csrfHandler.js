a7610c59e6a5a938c7ef84300c753bbe68d4c191
im6 <ee2win@gmail.com>
2019-09-08 10:58:16 -0400

    fix csrf middleware

---
+++ server/middlewares/csrfHandler.js
@@ -0,0 +1,25 @@
+import csrf from 'csurf';
+import get from 'lodash.get';
+import { CSRF_EXCEPTION, _DEV_ } from '../config';
+
+const csrfOrigin = csrf();
+export const csrfOverride = (...args) => {
+  // args will be [req, res, next]
+  const self = this;
+  if (get(args[0], 'body._csrf', null) === CSRF_EXCEPTION) {
+    console.log('csrf exception'); // eslint-disable-line no-console
+    args[2]();
+  } else {
+    csrfOrigin.apply(self, args);
+  }
+};
+
+export const csrfCookie = (req, res, next) => {
+  if (_DEV_) {
+    // eslint-disable-next-line no-console
+    console.log(`${req.method}: ${req.originalUrl}`);
+  } else {
+    res.cookie('_csrf', req.csrfToken());
+  }
+  next();
+};
