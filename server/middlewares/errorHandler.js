081fa5859f965b29101b381dc0320dfd2c863cdb
im6 <ee2win@gmail.com>
2019-09-19 20:53:38 -0400

    update error handler

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -4,20 +4,18 @@ import path from 'path';
 const { PWD } = process.env;
 const errorPage = path.resolve(PWD, './dist/error.html');
 
-export const onNotFound = (req, res, next) => {
-  console.error(`=====  resource(${req.url}) not found  =====`);
-  res.status(404).sendFile(errorPage);
+export const onNotFound = (req, res) => {
+  console.error(`Error: 404, ${req.url} not found`);
+  res.status(400).sendFile(errorPage);
 };
 
 export const onError = (err, req, res, next) => {
-  console.error('=====  Internal Error  =====');
-  console.log(err.toString());
-  const errorCode = typeof err === 'number' ? err : 400;
+  console.error(`Error: ${err.toString()}, ${req.url}`);
   if (req.method === 'POST') {
-    res.status(errorCode).json({
+    res.status(400).json({
       errors: true, // consistent with graphql error object
     });
   } else {
-    res.status(errorCode).sendFile(errorPage);
+    res.status(400).sendFile(errorPage);
   }
 };
