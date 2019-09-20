3d0f64a1fda15b4f6dcc4c38d8ed21b3260f3454
im6 <ee2win@gmail.com>
2019-09-19 22:00:33 -0400

    fix url

--- server/middlewares/staticFileProd.js
+++ server/middlewares/staticFileProd.js
@@ -1,22 +1,19 @@
 import path from 'path';
 
 const { PWD } = process.env;
-const scriptsList = ['robots.txt', 'sitemap.xml', 'favicon.ico'];
+const scriptsList = ['/robots.txt', '/sitemap.xml', '/favicon.ico'];
 
 const fileSet = scriptsList.reduce((acc, cur) => {
   acc[cur] = true;
   return acc;
 }, {});
-console.log('check');
+
 export default (req, res, next) => {
-  const fileName = req.url;
-  console.log(fileName);
-  if (fileName in fileSet) {
-    const filePath = path.resolve(PWD, `./dist/${fileName}`);
-    console.log(filePath);
+  const { url } = req;
+  if (url in fileSet) {
+    const filePath = path.resolve(PWD, `./dist${url}`);
     res.sendFile(filePath);
   } else {
-    console.log('prod 404');
     next(404);
   }
 };
