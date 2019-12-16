eb744228c35739c5dc51e6600eb2becf744dccdb
im6 <ee2win@gmail.com>
2019-12-16 00:21:03 -0500

    update the color epics

--- client/config/epic.js
+++ client/config/epic.js
@@ -3,11 +3,25 @@ import { combineEpics } from 'redux-observable';
 import { adminAnonymousColorEpic, adminAdjudicateEpic } from '../epics/admin';
 import { userAuthEpic, userLogOffEpic, userLoginEpic } from '../epics/user';
 
+import {
+  getInitColorsEpic,
+  getUserColorsEpic,
+  toggleLikeEpic,
+  colorShareEpic,
+  colorDownloadEpic,
+} from '../epics/color';
+
 export default combineEpics(
   adminAnonymousColorEpic,
   adminAdjudicateEpic,
 
   userAuthEpic,
   userLogOffEpic,
-  userLoginEpic
+  userLoginEpic,
+
+  getInitColorsEpic,
+  getUserColorsEpic,
+  toggleLikeEpic,
+  colorShareEpic,
+  colorDownloadEpic
 );
