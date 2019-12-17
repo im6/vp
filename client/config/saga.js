7097d4ab05dad00de5f1d307531de372c48ec66d
im6 <ee2win@gmail.com>
2019-12-16 22:14:41 -0500

    add image and replace saga

--- client/config/saga.js
+++
@@ -1,8 +0,0 @@
-const context = require.context('../sagas/', true, /\.js$/);
-const keys = context.keys();
-
-export default sagaMiddleware => {
-  keys.forEach(v => {
-    sagaMiddleware.run(context(v).default);
-  });
-};
