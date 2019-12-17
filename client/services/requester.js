1183b3ded6ef4a287c11de61bc212e8a556a85a8
im6 <ee2win@gmail.com>
2019-12-16 23:25:46 -0500

    fix requester

--- client/services/requester.js
+++ client/services/requester.js
@@ -4,7 +4,7 @@ import { ajax } from 'rxjs/ajax';
 const tokenElem = document.querySelector('#csrf');
 const { token: _csrf } = tokenElem.dataset;
 
-export default requester = body => {
+export default body => {
   return ajax({
     url: '/graphql',
     method: 'POST',
