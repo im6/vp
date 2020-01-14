4752902b23bf8520c694974846f4bb4a7367ab2f
im6 <ee2win@gmail.com>
2020-01-13 21:12:21 -0500

    add test case for user reducer

---
+++ src/client/reducers/user.spec.js
@@ -0,0 +1,58 @@
+import { Map, fromJS } from 'immutable';
+import userReducer from './user';
+
+describe('test user reducer behavior', () => {
+  test('action of user/logoff', () => {
+    expect(
+      userReducer(new Map({ detail: {}, facebookUrl: 'url' }), {
+        type: 'user/logoff',
+      })
+    ).toEqual(
+      Map({
+        detail: null,
+        facebookUrl: null,
+      })
+    );
+  });
+  test('action of user/auth/success', () => {
+    const userInfo = { name: 'tom' };
+    expect(
+      userReducer(new Map({ detail: {}, facebookUrl: 'url' }), {
+        type: 'user/auth/success',
+        payload: userInfo,
+      })
+    ).toEqual(
+      fromJS({
+        detail: userInfo,
+        facebookUrl: null,
+      })
+    );
+  });
+  test('action of user/auth/fail', () => {
+    const url = 'new url';
+    expect(
+      userReducer(new Map({ detail: {}, facebookUrl: 'url' }), {
+        type: 'user/auth/fail',
+        payload: url,
+      })
+    ).toEqual(
+      Map({
+        detail: null,
+        facebookUrl: url,
+      })
+    );
+  });
+  test('action of user/setLanguage', () => {
+    const lang = 'english';
+    expect(
+      userReducer(new Map({ lang: null }), {
+        type: 'user/setLanguage',
+        payload: lang,
+      })
+    ).toEqual(
+      Map({
+        lang,
+      })
+    );
+  });
+});
