350e302136a7108f437e20cbceba7a7f9224b823
im6 <ee2win@gmail.com>
2020-02-16 20:56:25 -0500

    add more case

---
+++ src/client/modules/newcolor/components/EditCanvas/ColorRow/index.spec.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ColorRow from '.';
+
+describe('render properly', () => {
+  const cb = jest.fn();
+  test('render color correct', () => {
+    const { rerender, container } = render(
+      <ColorRow onRowClick={cb} colorValue="#aaffee" />
+    );
+    rerender(<ColorRow onRowClick={cb} isActive />);
+    expect(
+      container.querySelector('div > div').style.backgroundColor
+    ).toBeFalsy();
+  });
+
+  test('shouldComponentUpdate', () => {
+    const { rerender, container } = render(
+      <ColorRow onRowClick={cb} colorValue="#" />
+    );
+    rerender(<ColorRow onRowClick={cb} colorValue="#000001" />);
+    rerender(<ColorRow onRowClick={cb} colorValue="#000001" isActive />);
+    expect(container.querySelector('div > div').style.backgroundColor).toBe(
+      'rgb(0, 0, 1)'
+    );
+  });
+});
