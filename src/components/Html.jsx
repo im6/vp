7102bcbb6f7bb58adbbf69c699079a715a302051
im6 <ee2win@gmail.com>
2020-02-27 19:08:23 -0500

    minify the gtm source

--- src/components/Html.jsx
+++ src/components/Html.jsx
@@ -41,21 +41,8 @@ const Html = ({
       <link href={`${style}?${version}`} rel="stylesheet" />
       <script
         dangerouslySetInnerHTML={{
-          __html: `if(window.location.hostname.indexOf('colorpk.com') > -1){
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5LX6LV3');
-
-            var initLoadTime = Date.now();
-            window.addEventListener('_COLORPK_SCRIPT_READY', function(){
-              window.dataLayer.push({
-                'scriptLoadingTime': Date.now() - initLoadTime
-              });
-              console.log('server last build: ${lastBuildDate}');
-            });
-          }`,
+          __html: `if(window.location.hostname.indexOf('colorpk.com') > -1){(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5LX6LV3');} var initLoadTime = Date.now();window.addEventListener('_COLORPK_SCRIPT_READY', function(){window.dataLayer.push({'scriptLoadingTime': Date.now() - initLoadTime});console.log('server last build: ${lastBuildDate}');});
+          `,
         }}
       />
     </head>
