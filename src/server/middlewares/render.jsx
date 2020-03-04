4ba8eb2615a22d38f35970fbc7eaa4bef1236494
im6 <ee2win@gmail.com>
2020-03-03 22:40:24 -0500

    update

--- src/server/middlewares/render.jsx
+++ src/server/middlewares/render.jsx
@@ -69,6 +69,6 @@ export default (req, res) => {
   );
   const html = renderToStaticMarkup(htmlDOM);
   res.status(200);
-  // res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
   res.send(`<!DOCTYPE html>${html}`);
 };
