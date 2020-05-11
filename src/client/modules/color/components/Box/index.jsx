1640a539fe27af6d9b33eac91eb088dca59f5cc4
im6 <ee2win@gmail.com>
2020-05-10 22:16:42 -0400

    update to func component

--- src/client/modules/color/components/Box/index.jsx
+++ src/client/modules/color/components/Box/index.jsx
@@ -1,71 +1,65 @@
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import style from './style.sass';
 import LikeButton from './components/LikeButton';
 import ColorCanvas from './components/ColorCanvas';
 
-class Box extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onLikeClick = this.onLikeClick.bind(this);
-    this.onCanvasClick = this.onCanvasClick.bind(this);
-  }
-
-  shouldComponentUpdate(nextProps) {
-    const { liked, boxInfo, vertical } = this.props;
-    return (
-      nextProps.liked !== liked ||
-      nextProps.boxInfo.get('id') !== boxInfo.get('id') ||
-      nextProps.vertical !== vertical
-    );
-  }
-
-  onLikeClick() {
-    const { liked, boxInfo, onLikeClick } = this.props;
+const Box = ({
+  liked,
+  boxInfo,
+  vertical,
+  showUsername,
+  onLikeClick,
+  onCanvasClick,
+}) => {
+  const onLikeClickLocal = () => {
     onLikeClick({
       willLike: !liked,
       id: boxInfo.get('id'),
     });
-  }
-
-  onCanvasClick() {
-    const { boxInfo, onCanvasClick } = this.props;
+  };
+  const onCanvasClickLocal = () => {
     onCanvasClick(boxInfo);
-  }
-
-  render() {
-    const { liked, boxInfo, vertical, showUsername } = this.props;
-    return (
-      <div className={style.box}>
-        <ColorCanvas
-          vertical={vertical}
-          colorValue={boxInfo.get('color')}
-          onClick={this.onCanvasClick}
-        />
-        <LikeButton
-          liked={liked}
-          likeNum={boxInfo.get('like')}
-          onToggle={this.onLikeClick}
-        />
-        {showUsername && boxInfo.get('username') && (
-          <p>{boxInfo.get('username')}</p>
-        )}
-      </div>
-    );
-  }
-}
+  };
+  return (
+    <div className={style.box}>
+      <ColorCanvas
+        vertical={vertical}
+        colorValue={boxInfo.get('color')}
+        onClick={onCanvasClickLocal}
+      />
+      <LikeButton
+        liked={liked}
+        likeNum={boxInfo.get('like')}
+        onToggle={onLikeClickLocal}
+      />
+      {showUsername && boxInfo.get('username') && (
+        <p>{boxInfo.get('username')}</p>
+      )}
+    </div>
+  );
+};
 
 Box.propTypes = {
   boxInfo: PropTypes.object.isRequired, // immutable object
-  onLikeClick: PropTypes.func.isRequired,
-  onCanvasClick: PropTypes.func.isRequired,
   liked: PropTypes.bool,
   vertical: PropTypes.bool,
   showUsername: PropTypes.bool,
+  onLikeClick: PropTypes.func.isRequired,
+  onCanvasClick: PropTypes.func.isRequired,
 };
 
 Box.defaultProps = {
   onCanvasClick: () => {},
 };
 
-export default Box;
+const isEqual = (prevProps, nextProps) => {
+  const { liked, boxInfo, vertical } = prevProps;
+  return (
+    nextProps.liked === liked &&
+    nextProps.boxInfo.get('id') === boxInfo.get('id') &&
+    nextProps.vertical === vertical
+  );
+};
+
+export default memo(Box, isEqual);
