b2fd3afe2f84a77a61c9be9176316ba6271c23e3
im6 <ee2win@gmail.com>
2020-05-10 22:26:33 -0400

    update

--- src/client/modules/newcolor/components/EditCanvas/ColorRow/index.jsx
+++ src/client/modules/newcolor/components/EditCanvas/ColorRow/index.jsx
@@ -1,42 +1,35 @@
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import style from './style.sass';
 
-class ColorRow extends React.Component {
-  shouldComponentUpdate(nextProps) {
-    const isSame =
-      nextProps.colorValue == this.props.colorValue &&
-      nextProps.isActive == this.props.isActive;
-    return !isSame;
-  }
+const ColorRow = ({ onRowClick, colorValue, isActive }) => {
+  const rowStyle =
+    colorValue && colorValue != '#'
+      ? {
+          backgroundColor: colorValue,
+        }
+      : {
+          border: `1px solid ${isActive ? '#1a4cb6' : '#cccccc'}`,
+          backgroundImage:
+            "url('data:image/png;base64,R0lGODdhCgAKAPAAAOXl5f///ywAAAAACgAKAEACEIQdqXt9GxyETrI279OIgwIAOw==')",
+        };
 
-  render() {
-    const { onRowClick, colorValue, isActive } = this.props;
-    const rowStyle =
-      colorValue && colorValue != '#'
-        ? {
-            backgroundColor: colorValue,
-          }
-        : {
-            border: `1px solid ${isActive ? '#1a4cb6' : '#cccccc'}`,
-            backgroundImage:
-              "url('data:image/png;base64,R0lGODdhCgAKAPAAAOXl5f///ywAAAAACgAKAEACEIQdqXt9GxyETrI279OIgwIAOw==')",
-          };
-
-    return (
-      <div
-        className={style.rowContainer}
-        style={rowStyle}
-        onClick={onRowClick}
-      />
-    );
-  }
-}
-
-ColorRow.propTypes = {
-  onRowClick: PropTypes.func.isRequired,
-  colorValue: PropTypes.string,
-  isActive: PropTypes.bool,
+  return (
+    <div className={style.rowContainer} style={rowStyle} onClick={onRowClick} />
+  );
 };
 
-export default ColorRow;
+ColorRow.propTypes = {
+  colorValue: PropTypes.string,
+  isActive: PropTypes.bool,
+  onRowClick: PropTypes.func.isRequired,
+};
+
+const isEqual = (prevProps, nextProps) => {
+  const isSame =
+    nextProps.colorValue == prevProps.colorValue &&
+    nextProps.isActive == prevProps.isActive;
+  return isSame;
+};
+
+export default memo(ColorRow, isEqual);
