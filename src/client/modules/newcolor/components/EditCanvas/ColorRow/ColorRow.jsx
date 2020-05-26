39983c18b05ec438f8ca07613842f18634a0692f
im6 <ee2win@gmail.com>
2020-05-26 00:45:03 -0400

    add more case

---
+++ src/client/modules/newcolor/components/EditCanvas/ColorRow/ColorRow.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from './style.sass';
+
+const ColorRow = ({ onRowClick, colorValue, isActive }) => {
+  const rowStyle =
+    colorValue && colorValue != '#'
+      ? {
+          backgroundColor: colorValue,
+        }
+      : {
+          border: `1px solid ${isActive ? '#1a4cb6' : '#cccccc'}`,
+          backgroundImage:
+            "url('data:image/png;base64,R0lGODdhCgAKAPAAAOXl5f///ywAAAAACgAKAEACEIQdqXt9GxyETrI279OIgwIAOw==')",
+        };
+
+  return (
+    <div className={style.rowContainer} style={rowStyle} onClick={onRowClick} />
+  );
+};
+
+ColorRow.propTypes = {
+  colorValue: PropTypes.string,
+  isActive: PropTypes.bool,
+  onRowClick: PropTypes.func.isRequired,
+};
+
+export default ColorRow;
--- src/client/modules/newcolor/components/EditCanvas/ColorRow/index.jsx
+++ src/client/modules/newcolor/components/EditCanvas/ColorRow/index.jsx
@@ -1,29 +1,5 @@
-import React, { memo } from 'react';
-import PropTypes from 'prop-types';
-import style from './style.sass';
-
-const ColorRow = ({ onRowClick, colorValue, isActive }) => {
-  const rowStyle =
-    colorValue && colorValue != '#'
-      ? {
-          backgroundColor: colorValue,
-        }
-      : {
-          border: `1px solid ${isActive ? '#1a4cb6' : '#cccccc'}`,
-          backgroundImage:
-            "url('data:image/png;base64,R0lGODdhCgAKAPAAAOXl5f///ywAAAAACgAKAEACEIQdqXt9GxyETrI279OIgwIAOw==')",
-        };
-
-  return (
-    <div className={style.rowContainer} style={rowStyle} onClick={onRowClick} />
-  );
-};
-
-ColorRow.propTypes = {
-  colorValue: PropTypes.string,
-  isActive: PropTypes.bool,
-  onRowClick: PropTypes.func.isRequired,
-};
+import { memo } from 'react';
+import ColorRow from './ColorRow';
 
 const isEqual = (prevProps, nextProps) => {
   const isSame =
