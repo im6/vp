05307901778036a258819c3918a87f3eb87fefd5
im6 <ee2win@gmail.com>
2020-07-17 21:55:16 -0400

    update hex validation, enforce 6 digits lowercase, output uppercase

--- src/util/index.spec.js
+++ src/util/index.spec.js
@@ -7,7 +7,8 @@ describe('isColorHex', () => {
     expect(isColorHex(null)).toBe(false);
     expect(isColorHex('###')).toBe(false);
     expect(isColorHex('#1d8696#22646f#2f5a60#ff8193')).toBe(true);
-    expect(isColorHex('#1da#22646f#2f5a60#ff8193')).toBe(true);
+    expect(isColorHex('#1D8696#22646f#2f5a60#ff8193')).toBe(false);
+    expect(isColorHex('#1da#22646f#2f5a60#ff8193')).toBe(false);
     expect(isColorHex('#1da#22646f#2f5a60#ff8193#')).toBe(false);
     expect(isColorHex('#1da#22646f#2f5a60#ff8193#ff8193')).toBe(false);
   });
