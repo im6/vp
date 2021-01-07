bc71a06031c5957e3f80cd3540ba45a7331dbfa5
im6 <ee2win@gmail.com>
2021-01-06 20:29:34 -0500

    update cookie update hook

--- src/context/Language/index.jsx
+++ src/context/Language/index.jsx
@@ -1,5 +1,5 @@
 import Cookies from 'js-cookie';
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { translation as allTranslation } from '../../translation';
 import { langSelectionKey } from '../../constant';
 
@@ -7,16 +7,12 @@ export const LanguageContext = createContext();
 
 export const LanguageProvider = ({ initLang, children }) => {
   const [lang, setLang] = useState(initLang);
+  useEffect(() => {
+    Cookies.set(langSelectionKey, lang, { expires: 180 });
+  }, [lang]);
+
   return (
-    <LanguageContext.Provider
-      value={[
-        allTranslation[lang],
-        (a) => {
-          setLang(a);
-          Cookies.set(langSelectionKey, a, { expires: 180 });
-        },
-      ]}
-    >
+    <LanguageContext.Provider value={[allTranslation[lang], setLang]}>
       {children}
     </LanguageContext.Provider>
   );
