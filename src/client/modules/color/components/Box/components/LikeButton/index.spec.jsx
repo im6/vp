831147212e9e601f4117a0242d077dfeef562e22
im6 <ee2win@gmail.com>
2021-01-24 15:30:34 -0500

    remove svg tooltip

--- src/client/modules/color/components/Box/components/LikeButton/index.spec.jsx
+++ src/client/modules/color/components/Box/components/LikeButton/index.spec.jsx
@@ -4,9 +4,10 @@ import LikeButton from '.';
 describe('render properly', () => {
   test('render color correct', () => {
     const clickFn = jest.fn();
-    const { container } = render(
-      <LikeButton starNum={2} starred onToggle={clickFn} />
+    const { container, rerender } = render(
+      <LikeButton starNum={2} starred={false} onToggle={clickFn} />
     );
+    rerender(<LikeButton starNum={2} starred onToggle={clickFn} />);
     fireEvent.click(container.querySelector('button'));
     expect(clickFn).toBeCalled();
   });
