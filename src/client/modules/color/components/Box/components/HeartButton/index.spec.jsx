16a1184ea97652d1989764af4ed2ad0843cfc223
im6 <ee2win@gmail.com>
2021-01-24 15:39:39 -0500

    rename heartBtn handler func

--- src/client/modules/color/components/Box/components/HeartButton/index.spec.jsx
+++ src/client/modules/color/components/Box/components/HeartButton/index.spec.jsx
@@ -5,9 +5,9 @@ describe('render properly', () => {
   test('render color correct', () => {
     const clickFn = jest.fn();
     const { container, rerender } = render(
-      <HeartButton starNum={2} starred={false} onToggle={clickFn} />
+      <HeartButton starNum={2} starred={false} onClick={clickFn} />
     );
-    rerender(<HeartButton starNum={2} starred onToggle={clickFn} />);
+    rerender(<HeartButton starNum={2} starred onClick={clickFn} />);
     fireEvent.click(container.querySelector('button'));
     expect(clickFn).toBeCalled();
   });
